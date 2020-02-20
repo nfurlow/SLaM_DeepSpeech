feb3f6c47555beb5db8df3f1a32b63f3adf3b433
nfurlow <nfurlow@ufl.edu>
2020-02-19 20:51:09 -0500

    Moved transcription to separate function

--- SLaM_DeepSpeech/error_utils/error_utils.py
+++ SLaM_DeepSpeech/error_utils/error_utils.py
@@ -27,44 +27,77 @@ def wordError(item):
     return wer(ground_truth_list, hypothesis_list)
 
 def phoneError(item):
-    librispeech_lexicon = getDict()
+
+    # separate outputfile path and ground_truth file path from item
     outputfile, ground_truth = item
+    # outputfile contains the list of interences made by the deepspeech model
+    # ground_truth contains the list of expected outputs
+    # these files should be in the same order
+
+    #initialize as empty lists
     hypothesis_list = []
     ground_truth_list = []
 
+    # open outputfile
     with open(outputfile, 'r') as reader:
+        # loop through each line in outputfile
         for line in reader.read().splitlines():
-            line_words = line.split(' ')
-            temp = ""
-            for element in line_words:
-                if (element in librispeech_lexicon):
-                    transcription = librispeech_lexicon.get(element)
-                    temp += transcription
-                else:
-                    hypothesis_list.append("\n")
-                    break
-                hypothesis_list.append(temp)
+
+            # assign temp variable with line transcription
+            temp = getTranscription(line)
+
+            # append complete transcription of the line to hypothesis_list
+            hypothesis_list.append(temp)
 
     with open(ground_truth, 'r') as reader:
         for line in reader.read().splitlines():
-            line_words = line.split(' ')
-            temp = ""
-            for element in line_words:
-                if (element in librispeech_lexicon):
-                    transcription = librispeech_lexicon.get(element)
-                    temp += transcription
-                else:
-                    ground_truth_list.append("\n")
-                    break
+
+            # assign temp variable with line transcription
+            temp = getTranscription(line)
+
+            # append complete transcription of the line to ground_truth_list
             ground_truth_list.append(temp)
+
+    # FIXME: prints added for debugging and should be removed
     for e in ground_truth_list:
         print(e + "\n")
     for e in hypothesis_list:
         print(e + "\n")
 
+    # use jiwer wer() fucntion to compute word error rate of phonemes
+    # transcriptions are represented as 'words' in the arpabet system
     return wer(ground_truth_list, hypothesis_list)
 
 
+def getTranscription(line):
+
+    # return a dictionary structure containing the librishpeech lexicon, see error_utils.getDict()
+    librispeech_lexicon = getDict()
+
+    # initialize as empty string
+    fullTranscription = ""
+
+    # split line at spaces and initialize as a list of words
+    line_words = line.split(' ')
+
+    # loop through each element of line_words
+    for element in line_words:
+
+        # check that element exists in dictionary
+        if (element in librispeech_lexicon):
+            # return transcription for element
+            elementTranscription = librispeech_lexicon.get(element)
+            # concatinate transcription to the end of temp
+            fullTranscription = fullTranscription + elementTranscription
+        else:
+            # if any word in the line does not exist in the dictionary,
+            # temp is assigned "*DNE*" and the loop ends
+            fullTranscription = "*DNE*"
+            break
+
+    return fullTranscription
+
+# returns librispeech lexicon dictionary
 def getDict():
     librispeech_lexicon = {}
 
@@ -76,15 +109,20 @@ def getDict():
         return librispeech_lexicon
 
 
-# The following code is from: http://hetland.org/coding/python/levenshtein.py
+# FIXME: remove levenshtein distance method after phone error works
+# or use in phone error if jiwer does not work
 
-# This is a straightforward implementation of a well-known algorithm, and thus
-# probably shouldn't be covered by copyright to begin with. But in case it is,
-# the author (Magnus Lie Hetland) has, to the extent possible under law,
-# dedicated all copyright and related and neighboring rights to this software
-# to the public domain worldwide, by distributing it under the CC0 license,
-# version 1.0. This software is distributed without any warranty. For more
-# information, see <http://creativecommons.org/publicdomain/zero/1.0>
+################################################################################
+# The following code is from: http://hetland.org/coding/python/levenshtein.py  #
+#                                                                              #
+# This is a straightforward implementation of a well-known algorithm, and thus #
+# probably shouldn't be covered by copyright to begin with. But in case it is, #
+# the author (Magnus Lie Hetland) has, to the extent possible under law,       #
+# dedicated all copyright and related and neighboring rights to this software  #
+# to the public domain worldwide, by distributing it under the CC0 license,    #
+# version 1.0. This software is distributed without any warranty. For more     #
+# information, see <http://creativecommons.org/publicdomain/zero/1.0>          #
+################################################################################
 
 def levenshtein(a, b):
     "Calculates the Levenshtein distance between a and b."
